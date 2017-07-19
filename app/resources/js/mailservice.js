0bf13e00a0088a8ce04f38d79460ed49ee4e3064
bas <bas.vandeuren@xt-i.com>
2017-07-19 22:40:02 +0200

    found the mailIssue (was submitting too soon and refreshing the page therefor no mail could be sent)

--- app/resources/js/mailservice.js
+++ app/resources/js/mailservice.js
@@ -4,22 +4,17 @@
 
 $(document).ready(function(){
 	$("#sendMailButton").click(function(e){
-	var toEmail = $("#email").val();
-	var toName = $("#name").val();
-	var pricePerFrak = $("#pricePerFrak").val();
-	var numberOfJudoFrakken = $("#numberOfJudoFrakken").val();
-	var total = pricePerFrak * numberOfJudoFrakken;
-	
+	e.preventDefault();
 		emailjs.send("gmail","ippondeals_order_template",{
-		  "to_email": toEmail,
-		  "to_name": toName,
-		  "number_of_judo_frakken": numberOfJudoFrakken, 
-		  "price_per_frak": pricePerFrak,
-		  "total": total
+		  "to_email": $("#email").val(),
+		  "to_name": $("#name").val(),
+		  "number_of_judo_frakken": $("#numberOfJudoFrakken").val(), 
+		  "price_per_frak": $("#pricePerFrak").val(),
+		  "total": $("#pricePerFrak").val() * $("#numberOfJudoFrakken").val()
 		})
 		.then(
-		  function(response) {
-			console.log("SUCCESS", response);
+		  function() {
+			console.log("SUCCESS");
 		  }, 
 		  function(error) {
 			console.log("FAILED", error);
