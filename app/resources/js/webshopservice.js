0ed803593b97aa3480c84a7a838cb45c5243bb4f
bas <bas.vandeuren@xt-i.com>
2017-08-08 22:56:36 +0200

    layout and jsoninfy this shit yeah yeah

--- app/resources/js/webshopservice.js
+++ app/resources/js/webshopservice.js
@@ -1,13 +1,26 @@
+function setChosenProductToCookie(e){
+	e.preventDefault();
+}
+
 $(document).ready(function(){
+	function createProduct(index, val){
+		var product = $("<div>", {id:val.id, "class":val.category + " webshopProduct"});
+		var link = $("<a>", {href: "#", onclick:"return setChosenProductToCookie(event)"})
+		var image = $("<img>", {"alt":"test", "src":"../resources/img/" + val.imgSrc, "class":"productImg"});
+		link.append(image);
+		product.append(link);
+		
+		return product;
+	}
+
 	$.getJSON( "../resources/json/webshop.json", function( data ) {
-	  var items = [];
-	  $.each( data, function( key, val ) {
-		items.push( "<li id='" + key + "'>" + val + "</li>" );
+	  var row;
+	  $.each( data, function( index, val ) {
+		var iteration = index + 1;
+		$("#shopForm").append(createProduct(index, val));
 	  });
-	}).done(function() {
-    console.log( "second success" );
-  })
-  .fail(function() {
-    console.log( "error" );
+	}).fail(function() {
+    console.log( "An error occurred during fetching webshop data, please try again later." );
+	//ToDo, popup a screen to contact the Sören.
   });
 });
