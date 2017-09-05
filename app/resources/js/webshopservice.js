4e2da4a6d6147a4d730457f7b395eaf8579255a5
basvandeuren <basvandeuren@msn.com>
2017-09-05 21:13:45 +0200

    layout take 1

--- app/resources/js/webshopservice.js
+++ app/resources/js/webshopservice.js
@@ -1,7 +1,7 @@
 var categories = [];
 
 function setChosenProductToCookie(e){
-	setCookie("chosenProduct",$(e.currentTarget).closest("div").attr("id"),10);
+	setCookie("chosenProduct",$(e.currentTarget).closest("div.webshopProduct").attr("id"),10);
 	window.location.href = './detail.html';
 	e.preventDefault();
 }
@@ -14,10 +14,21 @@ function changeCategory(elem, category) {
 	$(elem).removeClass("btn-default");
 	$("#shopForm .webshopProduct").each(function(index, val){
 		if($(val).attr("class").indexOf(category) < 0){
-			$(val).hide();
+			$(val).addClass("hidden");
 		} else {
-			$(val).show();
+			$(val).removeClass("hidden");
 		}
+	});
+}
+function elementsEqualHeight(elements){
+	var heighestHeight = 0;
+	$(elements).each(function(index, element){
+		if($(element).height() > heighestHeight){
+			heighestHeight = $(element).height();
+		}
+	});
+	$(elements).each(function(index, element){
+		$(element).height(heighestHeight);
 	});
 }
 
@@ -27,10 +38,18 @@ $(document).ready(function(){
 			categories.push(val.category);
 		}
 		var product = $("<div>", {id:val.id, "class":val.category + " webshopProduct"});
+		var imgDiv = $("<div>", {"class": "img"});
 		var link = $("<a>", {href: "#", onclick:"return setChosenProductToCookie(event)"})
 		var image = $("<img>", {"alt":"test", "src":"../resources/img/" + val.imgSrc, "class":"productImg"});
+		var contentDiv = $("<div>", {"class":"content"});
+		var name = $("<p>").html(val.name);
+		var price = $("<p>").html("Vanaf: €" + val.pricePerSize[0].price);
 		link.append(image);
-		product.append(link);
+		imgDiv.append(link);
+		contentDiv.append(name);
+		contentDiv.append(price);
+		product.append(imgDiv);
+		product.append(contentDiv);
 		
 		return product;
 	}
@@ -50,4 +69,8 @@ $(document).ready(function(){
 		console.log("An error occurred during fetching webshop data, please try again later.");
 		//ToDo, popup a screen to contact Sören.
 	});
+	
+	$(window).on("load", function(){
+		elementsEqualHeight($(".webshopProduct"));
+	})
 });
